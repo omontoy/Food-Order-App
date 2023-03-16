051a0acd79168c9c01ee36819e6c8d9da3ddb7dd
Orlando Montoya Benitez <orlando.montoyab@perficient.com>
2023-03-16 15:04:34 -0500

    Allow showing or closing modal depending on where user clicks

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClickHeader={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="meals" />
